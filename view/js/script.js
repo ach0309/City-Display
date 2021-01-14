199f561acc4d0bb12401dbd318cfc2abbb774389
ac <aeonchavez03@gmail.com>
2021-01-14 02:26:51 -0500

    call json, build table on js

--- view/js/script.js
+++ view/js/script.js
@@ -1,62 +1,102 @@
-
-
-/*function getLetter(){
-    var letter = document.getElementById('input').value;
-
-    if (letter === first letter of city)
-{
-        put state name in first item of row, then add all cities
-        that start with the letter 
-    }
-
-}*/ 
-
+//<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
 var searchInput = document.getElementById('input');
 var val='';
 searchInput.addEventListener('input', (event) => {
     val = event.target.value;
+    //console.log(val);
+    //if (val ) is not a letter continue
 })
 
-var jsonResults;
+//fetch data from JSON URL
+var jsonResults = [];
 var stringURL = 'https://jsonmock.hackerrank.com/api/cities/?city=' + val;
+console.log(stringURL);
 
-/*fetch(stringURL)
-.then(response => {
-   return response.json();
-})
-.then(data => console.log(data.data));
+$(function() {
+    $.getJSON(stringURL, function(data) {
+        //console.log(data.data);
+        var tr;
+        var r= data.data;
+        for (var i = 0; i < r.length; i++) {
+            //console.log(r[i].state);
+            tr = $('<tr/>');
+            if (i===0){
+                tr.append("<td>" + r[i].state + "</td>");
+            } else {
+                tr.append("<td>" + r[i].city + "</td>");
+            }
+        $('table').append(tr);    
+        }
+    });
+});
+
+/*    
+//build the table
+function buildTable(data){
+    var myTable = document.getElementById('table')
+    for (var i=0; i<data.length; i++){
+        if (i===0){
+            var row = <tr>
+                        <td>${data[i]}.state</td>
+                    </tr>
+            myTable.innerHTML +=row;
+        }
+        else{
+            row = <tr>
+                    <td>${data[i]}.cities</td>
+                </tr>
+            myTable.innerHTML +=row;
+        }
+
+    }
+}
 */
 
-fetch(stringURL)
-.then(response => {
-   return response.json();
-})
-.then(function(data){
-    jsonResults = data.data;
-})
-console.log(jsonResults)
-
-
-
-var list = document.getElementById('list');
-
-function setList(group){
-    clearList();
-    for (var city of group){
-
+/*
+//another idea
+function buildTable(data){
+    var tr;
+    for (var i = 0; i < data.length; i++) {
+    tr = $('<tr/>');
+    tr.append("<td>" + data[i][0] + "</td>");
+    tr.append("<td>" + data[i][1] + "</td>");
+    $('table').append(tr);
     }
-    if (group.length === 0){
-        setNoResults();
+}
+
+*/
+
+/*
+//build the table
+function buildTable(data){
+    var myTable = document.getElementById('table')
+    var row ='';
+    for (var i=0; i<data.length; i++){
+        if (i===0){
+            row += '<tr>';
+            row += '<td>' + value.state[i] + '</td>';
+            row += '</tr>';
+        }
+        else{
+            row += '<tr>';
+            row += '<td>' + value.city[i] + '</td>';
+            row += '</tr>';
+        }
+    table.innerHTML += row
     }
-
 }
+*/
 
-function clearList(){
-
-}
-
-function setNoResults(){
-
-}
-
+/*
+    $.ajax({
+        method: 'GET',
+        url: stringURL,
+        type: JSON,
+        success: function(response){
+            jsonResults = response.data;
+            console.log(jsonResults);
+            //buildTable(jsonResults);
+        }
+    });
+*/
