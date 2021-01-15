f15f83f43b29d7a1b2e8df0527f407f5199ea10c
ac <aeonchavez03@gmail.com>
2021-01-14 20:06:05 -0500

    adding tables

--- view/js/script.js
+++ view/js/script.js
@@ -2,8 +2,6 @@
 
 var searchInput = document.getElementById('input');
 
-var val='';
-
 searchInput.addEventListener('input', (event) => {
     const val = event.target.value;
     working(val);
@@ -18,104 +16,39 @@ function working(input){
 
     //console.log(stringURL);
 
-    $(function() {
-        $.getJSON(stringURL, function(data) {
-            console.log(data.data);
-            var cities='';
-            var r= data.data;
-            $.each(r, function(key,value){
-                cities += '<tr>';
-                cities += '<td>' + value.state + '</td>';
-                cities += '<td>' + value.city+ '</td>';
-                cities += '</tr>';
-            });
-        $('#tables').append(cities);
-        });
-
-    });
-
-
-
-
-
-/*
+     
     $(function() {
         $.getJSON(stringURL, function(data) {
             //console.log(data.data);
-            var tr;
+            var items='';
             var r= data.data;
-            for (var i = 0; i < r.length; i++) {
-                //console.log(r[i].state);
-                tr = $('<tr/>');
-                if (i===0){
-                    tr.append("<td>" + r[i].state + "</td>");
+            //get the amount of cities found
+            var valLen = r.length;
+            document.getElementById('numCities').innerHTML = valLen;
+
+            var dict = {};
+            //create an object {state: [cities]}
+            $.each(r, function(key,value){
+                if (value.state in dict){
+                    dict[value.state].push(value.city);
                 } else {
-                    tr.append("<td>" + r[i].city + "</td>");
+                    dict[value.state] = [value.city];
                 }
-            $('table').append(tr);    
-            }
+            });
+
+            //loop through object 
+            $.each(dict, function(key,value){
+                items += '<tr>';
+                items += '<td>' + key + '</td>';
+                //loop through values to get the cities for each key
+                for (let i=0; i<value.length; i++){
+                    items += '<td>'+ value[i] + '</td>';
+                }
+                items += '</tr>';
+            });
+        $('#tables').append(items);
         });
+
     });
-*/
-    /*    
-    //build the table
-    function buildTable(data){
-        var myTable = document.getElementById('table')
-        for (var i=0; i<data.length; i++){
-            if (i===0){
-                var row = <tr>
-                            <td>${data[i]}.state</td>
-                        </tr>
-                myTable.innerHTML +=row;
-            }
-            else{
-                row = <tr>
-                        <td>${data[i]}.cities</td>
-                    </tr>
-                myTable.innerHTML +=row;
-            }
 
-        }
-    }
-    //another idea
-    function buildTable(data){
-        var tr;
-        for (var i = 0; i < data.length; i++) {
-        tr = $('<tr/>');
-        tr.append("<td>" + data[i][0] + "</td>");
-        tr.append("<td>" + data[i][1] + "</td>");
-        $('table').append(tr);
-        }
-    }
-
-    //build the table
-    function buildTable(data){
-        var myTable = document.getElementById('table')
-        var row ='';
-        for (var i=0; i<data.length; i++){
-            if (i===0){
-                row += '<tr>';
-                row += '<td>' + value.state[i] + '</td>';
-                row += '</tr>';
-            }
-            else{
-                row += '<tr>';
-                row += '<td>' + value.city[i] + '</td>';
-                row += '</tr>';
-            }
-        table.innerHTML += row
-        }
-    }
-
-        $.ajax({
-            method: 'GET',
-            url: stringURL,
-            type: JSON,
-            success: function(response){
-                jsonResults = response.data;
-                console.log(jsonResults);
-                //buildTable(jsonResults);
-            }
-        });
-    */
 }
